a6d32e9d1ac07cc647f0ff22500546626c1d8cb0
michael maxwell <mikemax@outlook.com>
2018-10-20 20:33:06 -0400

    basic structure

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+#def index(request):
+#    return HttpResponse("<h2>hey this is be home to the API</h2>")
+
+
+def index(request):
+    return render(request, 'api/home.html')
