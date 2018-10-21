4034a420776b64c9c6586f8d3c805f2a01366731
michael maxwell <mikemax@outlook.com>
2018-10-20 21:14:22 -0400

    basic structure

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tnv9s-ko3l12^l*ljt#z=8&zt^u5z#mmymktb@!_8r8-uz-@1d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
